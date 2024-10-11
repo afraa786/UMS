68b7a3c889039071d6322f93ca65e4f816b218d9
afraa786 <shaikhafraabi@gmail.com>
2024-10-11 14:21:00 +0530

    courses model has made

--- backend/server/models.py
+++ backend/server/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Courses(models.Model):
+    image = models.ImageField( upload_to=  'courses', null=True, blank=True)
+    name =  models.CharField(max_length=100)
+    description = models.TextField()
+    fees = models.DecimalField(max_digits=15, decimal_places=2)
+    course_domain = models.CharField( max_length=50)
+
+
+    def __str__(self):
+        return self.name
