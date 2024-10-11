44832115ccb613d28d6dc7d18f50411e6c5d11f4
afraa786 <shaikhafraabi@gmail.com>
2024-10-11 18:07:08 +0530

    updated the proper model and admin just coarse css is remaining

--- backend/server/views.py
+++ backend/server/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Courses
 
 # Create your views here.
 
 
 def home(request):
     if request.method =='GET':
-        return render(request,'home.html')
+        courses =  Courses.objects.all()
+        context = {
+            'courses': courses,
+            }
+        return render(request,'home.html', context)
 
 def about(request):
     if request.method =='GET':
@@ -14,4 +19,8 @@ def about(request):
     
 def courses(request):
     if request.method =='GET':
-        return render(request,'courses.html')
+        courses =  Courses.objects.all()
+        context = {
+            'courses': courses,
+            }
+        return render(request,'courses.html',context)
